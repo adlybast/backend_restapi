c3b9f3d80c34726041b097c1b9cb51841b2f86ad
adrianus johan <adrianus@go-jek.com>
2017-07-21 22:24:00 +0700

    modified

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/backend_restapi/';
+$config['base_url'] = 'https://boxilearning.000webhostapp.com/?dir=backend_restapi/backend_restapi/';
 
 /*
 |--------------------------------------------------------------------------
